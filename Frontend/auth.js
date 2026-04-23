34474ea31c8e834cc70ea51a1235c416b6a4118a
Qedi-novara <tinashesithole35@gmail.com>
2026-04-23 06:29:43 +0200

    Update auth.js

--- Frontend/auth.js
+++ Frontend/auth.js
@@ -2,28 +2,12 @@ async function register() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  await fetch("https://https://novara-backend.onrender.com/register", {
-    method: "POST",
-    headers: {"Content-Type":"application/json"},
-    body: JSON.stringify({ email, password })
-  });
-
-  alert("User created");
-}
-
-async function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  const res = await fetch("https://YOUR_BACKEND/login", {
+  const res = await fetch("https://https://novara-backend.onrender.com/register", {
     method: "POST",
     headers: {"Content-Type":"application/json"},
     body: JSON.stringify({ email, password })
   });
 
   const data = await res.json();
-
-  localStorage.setItem("token", data.token);
-
-  window.location.href = "dashboard.html";
+  alert(data.message);
 }
